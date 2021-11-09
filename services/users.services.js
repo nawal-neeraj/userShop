ee4ef876f3d0c3d07424cb4efe8c0384561e94f4
nawal.bhengra@stackmybiz.com <nawal.bhengra@stackmybiz.com>
2021-11-09 17:36:02 +0530

    created controller and user services for follow the MVC

--- services/users.services.js
+++ services/users.services.js
@@ -2,12 +2,12 @@ var user = require('../modal/usermodel');
 var otpGenerator = require('../utils/otp-generater');
 var apiKey = require('../config/smsOtp.json');
 var key = apiKey.factor.API_KEY;
-var senderId = apiKey.factor.SENDER_ID
+var senderId = apiKey.factor.SENDER_ID;
 const TwoFactor = new (require('2factor'))(key);
 
 var genrateOtp = (async function (req, res, next) {
-    let userDetails = await user.findOne({ mobile: req.mobile })
-    var otpNum = otpGenerator.OTPpassword()
+    let userDetails = await user.findOne({ mobile: req.mobile });
+    var otpNum = otpGenerator.OTPpassword();
     if (userDetails) {
         return res.send({ status: false, message: 'user already exist' });
     }
@@ -19,9 +19,9 @@ var genrateOtp = (async function (req, res, next) {
 
     let smsSent = TwoFactor.sendTemplate([req.mobile], "StackOTP", [otpNum], senderId)
     if (smsSent) {
-        doc.otp = otpNum
+        doc.otp = otpNum;
         await doc.save();
-        return doc._id
+        return doc._id;
     }
     return res.send({ stauts: false, message: 'unable to send otp' });
 
@@ -29,7 +29,7 @@ var genrateOtp = (async function (req, res, next) {
 
 var checkOTP = (async function (otpNum, userId) {
     try {
-        let result = await user.findOne({ _id: userId, otp: otpNum })
+        let result = await user.findOne({ _id: userId, otp: otpNum });
         if (result) {
             result.status = "Active";
             result.save();
@@ -42,11 +42,11 @@ var checkOTP = (async function (otpNum, userId) {
 })
 
 var userSignin = (async function (userNm, userPass) {
-    let result = await user.findOne({ username: userNm, password: userPass })
+    let result = await user.findOne({ username: userNm, password: userPass });
     if (!result) {
-        return res.send({ status: false, message: "Username or Password did not match" })
+        return res.send({ status: false, message: "Username or Password did not match" });
     }
-    return result
+    return result;
 });
 
-module.exports = { genrateOtp, checkOTP, userSignin }
+module.exports = { genrateOtp, checkOTP, userSignin };
