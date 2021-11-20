ea779bd8847a54d814b411e5e6344c08b24d82dc
nawal.bhengra@stackmybiz.com <nawal.bhengra@stackmybiz.com>
2021-11-20 12:57:34 +0530

    check

--- utils/otp-generater.js
+++
@@ -1,6 +0,0 @@
-const otpGenerator = require('otp-generator');
-
-exports.OTPpassword = ()=>{
-    let otp = otpGenerator.generate(5, {alphabets:false, upperCase: false, specialChars: false });
-    return otp;
-}
